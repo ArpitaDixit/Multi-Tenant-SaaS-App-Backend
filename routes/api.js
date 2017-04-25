fa32e5c243ca1d356a6d30d4457f129e2c6a243a
ArpitaDixit <arpi.dixit91@gmail.com>
2017-04-25 05:31:36 -0700

    consolidating routes

--- routes/api.js
+++ routes/api.js
@@ -158,7 +158,53 @@ router.put('/order/:id', function(req, res) {
 				}
     		}
  });
-}); 
+});
+
+router.post('/order/:id/pay', function(req, res) {
+	var order_id = req.params.id;
+	var upadteOrder = { status : 'PAID',message:'Payment received!' };
+		Order.findOne({id:order_id}, function (err, post) {
+			if (err) {
+				var status = '{"status":"error","message":"Order Payment Rejected."}';
+				res.json(JSON.parse(status));
+			}
+			else{
+				if(post.length===0){
+					var status = '{"status":"error","message":"Order not found."}';
+					res.json(JSON.parse(status));
+				}
+				{
+					//console.log(post);
+				if ( post.status==='PLACED') {
+					//console.log('here');
+					Order.update({id:order_id},upadteOrder, function (err) {
+						if (err) console.log(err);
+						else{
+
+							Order.findOne({id:order_id}, function (err, post) {
+							if (err) {
+							var status = '{"status":"error","message":"Order Payment Rejected."}';
+							res.json(JSON.parse(status));
+							}
+							else
+							{
+								res.json(post);
+							brewCoffee();
+							}
+});							
+						}
+					});
+				}
+				else
+				{
+					var status = '{"status":"error","message":"Order Payment Rejected."}';
+					res.json(JSON.parse(status));
+				}
+				}
+				
+			}		
+  });	
+});
 //Order.methods(['get','put','post','delete']);
 //Order.register(router,'/orders');
 

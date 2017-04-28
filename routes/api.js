c0847510345bd8834838827c5f3276eca0bf664f
ArpitaDixit <arpi.dixit91@gmail.com>
2017-04-27 21:39:07 -0700

    content-type changes

--- routes/api.js
+++ routes/api.js
@@ -205,6 +205,11 @@ router.post('/order/:id/pay', function(req, res) {
 				
 			}		
   });	
+		res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials', false);
+    res.header('Access-Control-Max-Age', '86400');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
 });
 //Order.methods(['get','put','post','delete']);
 //Order.register(router,'/orders');

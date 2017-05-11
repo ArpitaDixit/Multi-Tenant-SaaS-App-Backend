9f6e71775498d9b4e5d548b6c26af03631e6e1ad
ArpitaDixit <arpi.dixit91@gmail.com>
2017-05-10 23:28:48 -0700

    New mongo cluster

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Order = require('./models/order')
 var cors = require('cors');
 
 //MongoDB
-mongoose.connect('mongodb://54.215.18.103/starbucks');
+mongoose.connect('mongodb://54.67.46.155/starbucks');
 
 //Express
 var app = express();
